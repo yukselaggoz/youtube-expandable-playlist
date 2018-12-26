41b4840e6ffe87f9788f293e71c0815165a21104
Yüksel Ağgöz <yukselaggoz@gmail.com>
2018-12-26 12:24:40 +0300

    Initial Commit

---
+++ YoutubePlaylist2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubePlaylist2
+//
+//  Created by Yüksel Ağgöz on 26.12.2018.
+//  Copyright © 2018 Yüksel Ağgöz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
